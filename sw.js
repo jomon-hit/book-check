258bce1bd441938dd2761d64ca8d4efbe502a276
jomon-hit <hit.tanuki@gmail.com>
2026-08-11 11:20:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // はまとしょ 蔵書点検 - Service Worker
 // ホーム画面に追加したアプリをオフラインでも起動できるようにするためのキャッシュ
-var CACHE_NAME = "shelf-check-cache-v3";
+var CACHE_NAME = "shelf-check-cache-v5";
 var CORE_ASSETS = [
   "./index.html",
   "./manifest.json",
